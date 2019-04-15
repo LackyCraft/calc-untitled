6f8dfec8e82e548798080293c9441a13d39bfc55
LackyCraft <zqxw1245@mail.ru>
2019-04-15 22:50:30 +0300

    Add files via upload

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <cmath>
 double eq01=0;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
 /*
  * При нажатии кнопок 0-9 вызываем слот diget_num
- * Именно благодаря такой реализации мы не создавали кучу однотипных методов, а брали 10 сигналов кнопок которые уже оборащаются к одному методу digets_num()
+ * Именно благодаря такой реализации мы не создавали кучу однотипных методов, а брали 10 сигналов кнопок которые уже оборазуются к одному методу digets_num()
  *
  * много методов
 */
@@ -27,12 +28,19 @@ connect(ui->pushButton_div,SIGNAL(clicked()),this,SLOT(math()));
 connect(ui->pushButton_plus,SIGNAL(clicked()),this,SLOT(math()));
 connect(ui->pushButton_minus,SIGNAL(clicked()),this,SLOT(math()));
 connect(ui->pushButton_x,SIGNAL(clicked()),this,SLOT(math()));
+connect(ui->pushButton_step,SIGNAL(clicked()),this,SLOT(math()));
 connect(ui->pushButton_AC,SIGNAL(clicked()),this,SLOT(operation()));
+connect(ui->pushButton_cos,SIGNAL(clicked()),this,SLOT(operation()));
+connect(ui->pushButton_sin,SIGNAL(clicked()),this,SLOT(operation()));
+connect(ui->pushButton_plus_and_minus,SIGNAL(clicked()),this,SLOT(operation()));
 
 ui->pushButton_div->setCheckable(true);
 ui->pushButton_plus->setCheckable(true);
 ui->pushButton_minus->setCheckable(true);
 ui->pushButton_x->setCheckable(true);
+//ui->pushButton_sin->setCheckable(true);
+//ui->pushButton_cos->setCheckable(true);
+//ui->pushButton_plus_and_minus->setCheckable(true);
 }
 
 MainWindow::~MainWindow()
@@ -59,7 +67,14 @@ void MainWindow::operation(){
     if(button->text() == "AC"){
       ui->status->setText(QString::number(0));
       eq01=0;
+    }else if(button->text() == "-/+"){
+    ui->status->setText(QString::number(-1*(ui->status->text()).toDouble(),'g',10));
     }
+    else if(button->text() == "sin"){
+        ui->status->setText(QString::number(sin((ui->status->text()).toDouble()),'g',10));
+        }else if(button->text() == "cos"){
+        ui->status->setText(QString::number(cos((ui->status->text()).toDouble()),'g',10));
+        }
 }
 
 
@@ -78,7 +93,8 @@ void MainWindow::math()
 void MainWindow::on_pushButton_eq_clicked()
 {
  //QPushButton *button = static_cast<QPushButton*>(sender());
- double num_s;
+ double num_s,num_step;//num_s - сейчас на экране, степень это то что было
+ num_step=eq01;
     num_s=ui->status->text().toDouble();
     if(ui->pushButton_plus->isChecked()){
     ui->status->setText(QString::number(num_s+eq01,'g',10));
@@ -92,9 +108,12 @@ void MainWindow::on_pushButton_eq_clicked()
     }else if(ui->pushButton_x->isChecked()){
     ui->status->setText(QString::number(eq01*num_s,'g',10));
     ui->pushButton_x->setChecked(false);
+    }else if(ui->pushButton_step->isChecked()){
+        if(num_s != 0 ){
+        for(double i=num_s;i>1;i--)
+            eq01*=num_step;
+    ui->status->setText(QString::number(eq01));}
+        else{ui->status->setText(QString::number(1));}
+        ui->pushButton_x->setChecked(false);
     }
-
-
-
-
 }
